6bec48fde193eaee5587301828983fd37702d780
Paul Tennent <paultennent@gmail.com>
2018-07-06 12:53:07 +0100

    unity test for rovr

---
+++ Assets/ListInputs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+
+public class ListInputs : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int c = 0; c < 20; c++)
+        {
+            SteamVR_Controller.Device device = SteamVR_Controller.Input(c);
+            bool buttons = device.GetPress(0xffff);
+            Vector2 axis = device.GetAxis(EVRButtonId.k_EButton_Axis0);
+            Vector2 axis2 = device.GetAxis(EVRButtonId.k_EButton_Axis1);
+            if (axis.x != 0 || axis.y != 0)
+            {
+
+                print(c + ":" + axis.x + "," + axis.y + ":" + axis2.x + "," + axis2.y + "[" + buttons + "]");
+            }
+        }
+
+    }
+}
---
+++ Assets/SimpleMoveScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+
+public class SimpleMoveScript : MonoBehaviour {
+
+    public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input(1);
+        Vector2 axis = device.GetAxis(EVRButtonId.k_EButton_Axis0);
+        Vector3 move = cam.transform.forward * axis.y*Time.deltaTime*10f;
+        move= Vector3.ProjectOnPlane(move, Vector3.up);
+        transform.position += move;
+
+    }
+}
